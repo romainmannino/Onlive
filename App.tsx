9e31b7c8e634e5e1d9fa27f084d7ad59ecb99659
romainmannino <romainmannino@gmail.com>
2026-08-18 15:56:08 +0200

    Respect device bottom safe area

--- App.tsx
+++ App.tsx
@@ -1,1 +1,13 @@
-export { default } from './src/Root';
+import React from 'react';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import Root from './src/Root';
+
+export default function App(){
+  return(
+    <SafeAreaProvider>
+      <SafeAreaView style={{flex:1}} edges={['bottom']}>
+        <Root/>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
