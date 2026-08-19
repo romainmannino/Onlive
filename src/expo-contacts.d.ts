e13df40dc588c993dbd89768446539f5f07644ad
romainmannino <romainmannino@gmail.com>
2026-08-19 23:50:25 +0200

    Fix Expo Contacts typings used by Onlive

--- src/expo-contacts.d.ts
+++ src/expo-contacts.d.ts
@@ -2,6 +2,8 @@ import 'expo-contacts';
 
 declare module 'expo-contacts' {
   interface Contact {
-    id: string;
+    id?: string;
+    name?: string;
+    phoneNumbers?: Array<{ number?: string | null }>;
   }
 }
